db325957ca7fdd5e8f2853340dc371e1334cb104
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-14 15:56:51 -0400

    get the movies that N.Cage played in them

--- src/App.js
+++ src/App.js
@@ -31,13 +31,20 @@ function App() {
 
     fetchData();
   }, []);
-
+  //first find the id of Keanu Reeves as that's the piece in common between the endpoints. 
+  //then filter movies endpoint based on the id to only save movies K.Reeves played in
   const reeves = actors.filter((actor) => actor.name === "Keanu Reeves")
   const reevesId = reeves[0]?.actorId
-  console.log("id",reevesId)
+  console.log("reeves id",reevesId)
   const reevesMovies = movies.filter((movie) => movie.actors.includes(reevesId));
-  console.log("movies",reevesMovies)
+  console.log("reeves movies",reevesMovies)
 
+  //do the same for N.Cage
+  const cage = actors.filter((actor) => actor.name === "Nicolas Cage")
+  const cageId = cage[0]?.actorId
+  console.log("cage id",cageId)
+  const cageMovies = movies.filter((movie) => movie.actors.includes(cageId))
+  console.log("cage movies",cageMovies)
 
   
   return (
