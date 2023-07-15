42d74b0ac4662d113a5859a749ea03f33635f99b
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-15 10:51:30 -0400

    added style to the table

--- src/App.js
+++ src/App.js
@@ -35,43 +35,53 @@ function App() {
   //then filter movies endpoint based on the id to only save movies K.Reeves played in
   const reeves = actors.filter((actor) => actor.name === "Keanu Reeves")
   const reevesId = reeves[0]?.actorId
-  console.log("reeves id",reevesId)
+  console.log("Reeves' id",reevesId)
   const reevesMovies = movies.filter((movie) => movie.actors.includes(reevesId));
-  console.log("reeves movies",reevesMovies)
+  console.log("Reeves' movies",reevesMovies)
 
   //do the same for N.Cage
   const cage = actors.filter((actor) => actor.name === "Nicolas Cage")
   const cageId = cage[0]?.actorId
-  console.log("cage id",cageId)
+  console.log("Cage's id",cageId)
   const cageMovies = movies.filter((movie) => movie.actors.includes(cageId))
-  console.log("cage movies",cageMovies)
+  console.log("Cage's movies",cageMovies)
 
   //find actors whom played in their movies
-const commonActorIds = new Set();
-for (let i = 0; i < cageMovies.length; i++) {
-  for (let j = 0; j < reevesMovies.length; j++) {
-    const commonActors = cageMovies[i].actors.filter((actorId) =>
-      reevesMovies[j].actors.includes(actorId)
-    );
-    commonActors.forEach((actorId) => commonActorIds.add(actorId));
+  const commonActorIds = new Set();
+  for (let i = 0; i < cageMovies.length; i++) {
+    for (let j = 0; j < reevesMovies.length; j++) {
+      const commonActors = cageMovies[i].actors.filter((actorId) =>
+        reevesMovies[j].actors.includes(actorId));
+      commonActors.forEach((actorId) => commonActorIds.add(actorId));
+    }
   }
-}
+  console.log("commonActorIds",commonActorIds)
 
-// Convert the common actor IDs to actor objects
-const foundActors = Array.from(commonActorIds).map((actorId) =>
-  actors.find((actor) => actor.actorId === actorId)
-);
+  //Convert the common actor IDs to actor objects
+  const foundActors = Array.from(commonActorIds).map((actorId) =>
+    actors.find((actor) => actor.actorId === actorId)
+  );
   console.log("Actors who have played with both Cage and Reeves:", foundActors);
   
   return (
     <div className="App">
       <header className="App-header">
+          <h3>Actors who have played with both<br/>Nicolas Cage and Keanu Reeves:</h3>
           <div className='card'>
-            <h3>Actors who have played with both<br/>Nicolas Cage and Keanu Reeves:</h3>
-            <ul>
-              {foundActors.map((actor) => <li>{"Name: "}{actor.name}, {"ID: "}{actor.actorId}</li>)}
-            </ul>
-          </div>   
+            <div className='table'>
+              <div className='row header'>
+                <div className='column'>ID</div>
+                <div className='column'>Name</div>
+              </div>
+              <hr></hr>
+              {foundActors.map((actor) => (
+                <div className='row' key={actor.actorId}>
+                  <div className='column'>{actor.actorId}</div>
+                  <div className='column'>{actor.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>  
       </header>
     </div>
   );
