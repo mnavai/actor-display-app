42d7e21df33e13edeba97efc91608b40c904e18a
Maryam Navaei <maryam.navaei@gmail.com>
2023-07-15 16:46:41 -0400

    latest changes

--- src/App.js
+++ src/App.js
@@ -32,34 +32,6 @@ function App() {
 
     fetchData();
   }
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const actorsResponse = await fetch('https://switch-yam-equator.azurewebsites.net/api/actors', {
-  //         headers: {
-  //           'x-chmura-cors': '65cbd27c-860e-4950-8dc9-02b24e4d9020'
-  //         }
-  //       });
-  //       const actorsData = await actorsResponse.json();
-  //       setActors(actorsData);
-  //       console.log('Actors:', actorsData);
-  //       const moviesResponse = await fetch('https://switch-yam-equator.azurewebsites.net/api/movies', {
-  //         headers: {
-  //           'x-chmura-cors': '65cbd27c-860e-4950-8dc9-02b24e4d9020'
-  //         }
-  //       });
-  //       const moviesData = await moviesResponse.json();
-  //       setMovies(moviesData);
-  //       console.log('Movies:', moviesData);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-
   //first find the id of Keanu Reeves as that's the piece in common between the endpoints. 
   //then filter movies endpoint based on the id to only save movies K.Reeves played in
   const reeves = actors.filter((actor) => actor.name === "Keanu Reeves")
