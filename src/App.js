ace364e3b68ef02c194dc71cb1c0017b58d26bcf
Maryam Navaei <maryam.navaei@gmail.com>
2023-08-05 19:45:55 -0400

    fixed the issue with the object

--- src/App.js
+++ src/App.js
@@ -36,6 +36,65 @@ function App() {
     fetchData();
   }
 
+// {
+// "Name": "Alan Smithee",
+// "KRMovies": [
+    // "The Matrix",
+    // "The Matrix Revolutions"
+    // ]
+// "NCMovies": [
+    // "Gone in Sixty Seconds"
+// ]}
+
+  const btnClassName = buttonClicked ? 'submit-btn clicked' : 'submit-btn';
+  const btnClassNamePost = postButtonClicked ? 'submit-btn clicked' : 'submit-btn';
+  //first find the id of Keanu Reeves as that's the piece in common between the endpoints. 
+  //then filter movies endpoint based on the id to only save movies K.Reeves played in
+  const reeves = actors.filter((actor) => actor.name === "Keanu Reeves")
+  const reevesId = reeves[0]?.actorId
+  const reevesMovies = movies.filter((movie) => movie.actors.includes(reevesId));
+  console.log("reevesMovies",reevesMovies)
+ 
+  //do the same for N.Cage
+  const cage = actors.filter((actor) => actor.name === "Nicolas Cage")
+  const cageId = cage[0]?.actorId
+  const cageMovies = movies.filter((movie) => movie.actors.includes(cageId))
+  console.log("cagemov",cageMovies)
+  // Find actors who played in their movies.
+  const commonActorIds = cageMovies.reduce((commonActorIds, cageMovie) => {
+    const actorsInBothMovies = cageMovie.actors.filter((actorId) =>
+      reevesMovies.some((reevesMovie) => reevesMovie.actors.includes(actorId))
+    );
+    return [...new Set([...commonActorIds, ...actorsInBothMovies])];
+  }, []);
+ 
+  // Convert the common actor IDs to actor objects.
+  const foundActors = commonActorIds.map((actorId) =>
+    actors.find((actor) => actor.actorId === actorId));
+  console.log("Actors who have played with both Cage and Reeves:", foundActors);
+
+  const actorsData = foundActors.map((actor) => {
+    // eslint-disable-next-line array-callback-return
+    const krMovies = reevesMovies.filter((movie) => {
+      if(movie.actors.includes(actor.actorId)){ return movie.title }
+    });
+    // eslint-disable-next-line array-callback-return
+    const ncMovies = cageMovies.filter((movie) => {
+      if(movie.actors.includes(actor.actorId)){ 
+        return movie.title 
+      }
+    });
+    
+    console.log("krMovies",krMovies)
+    return {
+      Name: actor.name,
+      KRMovies: krMovies.map((movie) => movie.title),
+      NCMovies: ncMovies.map((movie) => movie.title)
+    };
+  })
+
+  console.log("actor data", actorsData)  
+
   const handlePost = () => {
     const headers = {
       'Content-Type': 'application/json',
@@ -48,6 +107,7 @@ function App() {
       body: JSON.stringify(foundActors)
     })
       .then((response) => {
+        console.log("respones is",response)
         if (!response.ok) throw new Error(response.status);
         else {
           console.log(response.ok)
@@ -64,33 +124,7 @@ function App() {
         // Handle the error
       });
   };
-
-  const btnClassName = buttonClicked ? 'submit-btn clicked' : 'submit-btn';
-  const btnClassNamePost = postButtonClicked ? 'submit-btn clicked' : 'submit-btn';
-  //first find the id of Keanu Reeves as that's the piece in common between the endpoints. 
-  //then filter movies endpoint based on the id to only save movies K.Reeves played in
-  const reeves = actors.filter((actor) => actor.name === "Keanu Reeves")
-  const reevesId = reeves[0]?.actorId
-  const reevesMovies = movies.filter((movie) => movie.actors.includes(reevesId));
- 
-  //do the same for N.Cage
-  const cage = actors.filter((actor) => actor.name === "Nicolas Cage")
-  const cageId = cage[0]?.actorId
-  const cageMovies = movies.filter((movie) => movie.actors.includes(cageId))
-
-  // Find actors who played in their movies.
-  const commonActorIds = cageMovies.reduce((commonActorIds, cageMovie) => {
-    const actorsInBothMovies = cageMovie.actors.filter((actorId) =>
-      reevesMovies.some((reevesMovie) => reevesMovie.actors.includes(actorId))
-    );
-    return [...new Set([...commonActorIds, ...actorsInBothMovies])];
-  }, []);
-
-  // Convert the common actor IDs to actor objects.
-  const foundActors = commonActorIds.map((actorId) =>
-    actors.find((actor) => actor.actorId === actorId));
-  console.log("Actors who have played with both Cage and Reeves:", foundActors);
-  
+  console.log("foundactors",foundActors)
   return (
     <div className="App">
       <div className="App-header">
@@ -98,16 +132,32 @@ function App() {
           <div className='card'>
             <div className='table'>
               <div className='row header'>
-                <div className='column-id'>ID</div>
                 <div className='column-name'>Name</div>
               </div>
               <hr></hr>
               {foundActors.map((actor) => (
-                <div className='row' key={actor.actorId}>
-                  <div className='column-id'>{actor.actorId}</div>
-                  <div className='column-name'>{actor.name}</div>
-                </div>
-              ))}
+    <div className="row" key={actor.actorId}>
+      <div className="column-name">{actor.name}</div>
+      <div className="column-kr-mov">
+        <ul>
+          {reevesMovies
+            .filter((movie) => movie.actors.includes(actor.actorId))
+            .map((movie) => (
+              <li key={movie.movieId}>{movie.name}</li>
+            ))}
+        </ul>
+      </div>
+      <div className="column-nc-mov">
+        <ul>
+          {cageMovies
+            .filter((movie) => movie.actors.includes(actor.actorId))
+            .map((movie) => (
+              <li key={movie.movieId}>{movie.name}</li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  ))}
             </div>
           </div> 
           <div className='btn'>
